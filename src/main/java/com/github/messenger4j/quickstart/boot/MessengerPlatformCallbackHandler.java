9382a6a5e58a9fffcf12990251538c5b44b0dec9
ilyeskhamlichi <ilyes@khamlichi.net>
2017-10-12 23:16:08 +0200

    "added text char limit"

--- src/main/java/com/github/messenger4j/quickstart/boot/MessengerPlatformCallbackHandler.java
+++ src/main/java/com/github/messenger4j/quickstart/boot/MessengerPlatformCallbackHandler.java
@@ -327,7 +327,7 @@ public class MessengerPlatformCallbackHandler {
     }
 
     private void sendTextMessage(String recipientId, String text) {
-        text = text.substring(0, 300);
+        text = text.substring(0, Math.min(text.length(), 300));
         try {
             final Recipient recipient = Recipient.newBuilder().recipientId(recipientId).build();
             final NotificationType notificationType = NotificationType.REGULAR;
